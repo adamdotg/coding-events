5127d184f591cf5f076bcabb27177a17a25d0fc5
adamdotg <adamdotg@gmail.com>
2020-02-17 23:12:58 -0600

    Edit works with Jane's EventController. Mine is broken somewhere.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,4 @@
 package org.launchcode.codingevents.controllers;
-
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.launchcode.codingevents.models.EventType;
@@ -7,25 +6,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
-
-/**
- * Created by Chris Bay
- */
 @Controller
 @RequestMapping("events")
 public class EventController {
-
+    //private static List<Event> events = new ArrayList<>();
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
         model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
-
+    //    lives at /events/create
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
@@ -33,47 +27,130 @@ public class EventController {
         model.addAttribute("types", EventType.values());
         return "events/create";
     }
-
     @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
-                                         Errors errors, Model model) {
-        if(errors.hasErrors()){
+    public String createEvent(@ModelAttribute  @Valid Event newEvent, Errors errors, Model model) {
+        if (errors.hasErrors()) {
             model.addAttribute("title", "Create Event");
+            model.addAttribute("types", EventType.values());
             return "events/create";
         }
         EventData.add(newEvent);
         return "redirect:";
     }
-
     @GetMapping("delete")
-    public String displayDeleteEventForm(Model model){
+    public String displayDeleteEventForm(Model model) {
         model.addAttribute("title", "Delete Events");
         model.addAttribute("events", EventData.getAll());
         return "events/delete";
     }
-
     @PostMapping("delete")
-    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+    public String deleteEvent(@RequestParam(required = false) int[] eventIds) {
+        System.out.println(eventIds);
         if (eventIds != null) {
-            for (int id : eventIds) {
+            for (int id: eventIds) {
                 EventData.remove(id);
             }
         }
         return "redirect:";
     }
-    //New handler methods------------------
-//    @GetMapping("edit")
-//    public String displayEditForm(Model model, @PathVariable int eventId){
-//        EventData.getById(eventId);
-//        model.addAttribute("id", "Edit Events");//what is attribute name and value
+
+
+
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        model.addAttribute("title", "Edit Event");
+        model.addAttribute("event", EventData.getById(eventId));
+        model.addAttribute("types", EventType.values());
+        return "events/edit";
+    }
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description) {
+        // controller code will go here
+        return "events/edit";
+    }
+}
+
+//MY CODE BELOW THAT DOESNT WORK FOR SOME REASON
+//package org.launchcode.codingevents.controllers;
 //
-//        return "events/edit";
+//import org.launchcode.codingevents.data.EventData;
+//import org.launchcode.codingevents.models.Event;
+//import org.launchcode.codingevents.models.EventType;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.validation.Errors;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.validation.Valid;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * Created by Chris Bay
+// */
+//@Controller
+//@RequestMapping("events")
+//public class EventController {
+//
+//    private static List<Event> events = new ArrayList<>();
+//
+//    @GetMapping
+//    public String displayAllEvents(Model model) {
+//        model.addAttribute("title", "All Events");
+//        model.addAttribute("events", EventData.getAll());
+//        return "events/index";
+//    }
+//
+//    @GetMapping("create")
+//    public String displayCreateEventForm(Model model) {
+//        model.addAttribute("title", "Create Event");
+//        model.addAttribute(new Event());
+//        model.addAttribute("types", EventType.values());
+//        return "events/create";
+//    }
+//
+//    @PostMapping("create")
+//    public String createEvent(@ModelAttribute @Valid Event newEvent,
+//                                         Errors errors, Model model) {
+//        if(errors.hasErrors()){
+//            model.addAttribute("title", "Create Event");
+//            model.addAttribute("types", EventType.values());
+//            return "events/create";
+//        }
+//        EventData.add(newEvent);
+//        return "redirect:";
+//    }
+//
+//    @GetMapping("delete")
+//    public String displayDeleteEventForm(Model model){
+//        model.addAttribute("title", "Delete Events");
+//        model.addAttribute("events", EventData.getAll());
+//        return "events/delete";
+//    }
+//
+//    @PostMapping("delete")
+//    public String deleteEvent(@RequestParam(required = false) int[] eventIds) {
+//        if (eventIds != null) {
+//            for (int id : eventIds) {
+//                EventData.remove(id);
+//            }
+//        }
+//        return "redirect:";
+//    }
+//
+//    @GetMapping("edit/{eventId}")
+//    public String displayEditForm(Model model, @PathVariable int eventId){
+//        //EventData.getById(eventId);
+//        model.addAttribute("title", "Edit Event");
+//        model.addAttribute("event", EventData.getById(eventId));
+//        model.addAttribute("types", EventType.values());
+//        return "events/edit/";
 //    }
 //
 //    @PostMapping("edit")
 //    public String processEditForm(int eventId, String name, String description){
 //
-//
+//    return "events/edit";
 //
 //    }
-}
+//}
